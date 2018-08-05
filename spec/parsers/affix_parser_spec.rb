1950567302006cbac7cca3fb9bdd31450551f324
Anton Frolov <frol.mr@ya.ru>
2018-08-05 20:48:46 +0300

    basic spec config

---
+++ spec/parsers/affix_parser_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../lib/parsers/affix_parser'
+
+RSpec.describe Parsers::AffixParser do
+  context 'validations' do
+    context 'validate input file extension' do
+      it 'raise exception when file extension is wrong' do
+        input_file = File.new(Dir.pwd + '/spec/dictionaries/test_file.wrong_extension')
+        expect { Parsers::AffixParser.new(input_file).parse }.to raise_error(ExtensionError)
+      end
+    end
+  end
+end
