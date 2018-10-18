67f6760144b553facb6a63cfea02775a0925cf60
Anton Frolov <frol.mr@ya.ru>
2018-10-18 12:31:34 +0300

    remove commented code

--- spec/dictionary_reader_spec.rb
+++ spec/dictionary_reader_spec.rb
@@ -8,42 +8,6 @@ RSpec.describe DictionaryReader do
 
   subject { described_class.read(aff_path, dic_path) }
 
-  # context 'unsuccessfull cases:' do
-  #   context 'file doesn\'t exist' do
-  #     let(:aff_path) { 'this/file/doesnt/exist' }
-  #     let(:dic_path) { 'this/file/doesnt/exist' }
-
-  #     it 'returns no_such_file error' do
-  #       expect { subject }.to raise_error(Errno::ENOENT, /No such file or directory/)
-  #     end
-  #   end
-
-  #   context 'input wrong type' do
-  #     let(:aff_path) { :some_symbol }
-  #     let(:dic_path) { :some_symbol }
-
-  #     it 'returns type error' do
-  #       expect { subject }.to raise_error(TypeError, /no implicit conversion of/)
-  #     end
-  #   end
-
-  #   context 'file not readable' do
-  #     let(:args) { './spec/dictionaries/unreadable_file' }
-
-  #     before do
-  #       FileUtils.chmod 'u=,go=', args
-  #     end
-
-  #     after do
-  #       FileUtils.chmod 'u=wr,go=rr', args
-  #     end
-
-  #     it 'returns permission error' do
-  #       expect { subject }.to raise_error(Errno::EACCES, /Permission denied/)
-  #     end
-  #   end
-  # end
-
   context 'successfull cases' do
     let(:aff_path) { './spec/dictionaries/test_affix.aff' }
     let(:dic_path) { './spec/dictionaries/test_dict.dic' }
